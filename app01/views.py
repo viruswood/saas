1e696cd7fa7e02d6cda268086efa898ba5fa548e
viruswood <wood960916@163.com>
2020-10-14 22:44:02 +0800

    bootstrap优化美化页面

--- app01/views.py
+++ app01/views.py
@@ -24,14 +24,23 @@ from django.core.validators import RegexValidator
 
 
 class RegisterModelForm(forms.ModelForm):
-    mobile_phone = forms.CharField(label="手机号", validators=[RegexValidator(r'^(1[3|4|5|6|7|8|9])\d{9}$', '手机号格式错误')])
-    password = forms.CharField(label='密码', widget=forms.PasswordInput)
-    confirm_password = forms.CharField(label='确认密码', widget=forms.PasswordInput)
+    mobile_phone = forms.CharField(label="手机号", validators=[RegexValidator(r'^(1[3|4|5|6|7|8|9])\d{9}$', '手机号格式错误')],
+                                   widget=forms.TextInput())
+    password = forms.CharField(label='密码',
+                               widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label='确认密码', widget=forms.PasswordInput(
+    ))
     code = forms.CharField(label='验证码')
 
     class Meta:
         model = models.UserInfo
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, filed in self.fields.items():
+            filed.widget.attrs['class'] = 'form-control'
+            filed.widget.attrs['placeholder'] = '请输入{}'.format(filed.label)
 
 
 def register(request):
