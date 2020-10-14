2fea0c82c388d3c4b7ebb169340778e1ba54ef86
viruswood <wood960916@163.com>
2020-10-14 20:39:56 +0800

    生成requirements.txt文件

--- sass/settings.py
+++ sass/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
